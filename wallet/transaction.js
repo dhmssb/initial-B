fcd86f8dae6e725eeaf38e6e3a78fc35619ba2f1
dhmssb <dhmssb@192.168.1.11>
2024-05-05 17:17:50 +0700

    validating transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,10 +1,29 @@
 const uuid = require('uuid')
+const {verifySignature} = require('../util')
 
 class Transaction {
     constructor({senderWallet, recipient, amount}) {
         this.id = uuid
         this.outputMap = this.createOutputMap({senderWallet, recipient, amount})
         this.input = this.createInput({senderWallet, outputMap: this.outputMap})
+    }
+
+    static validTransaction(transaction) {
+        const {input: {address, amount, signature}, outputMap} = transaction
+        const outputTotal = Object.values(outputMap).reduce((total, outputAmount) => total + outputAmount)
+
+        if (amount !== outputTotal) {
+            console.error(`invalid tx from ${address}`)
+            return false
+        }
+
+        if (!verifySignature({publicKey: address, data: outputMap, signature})) {
+            console.error(`invalid from signature ${address}`)
+            return false
+        }
+
+
+        return true
     }
 
     createOutputMap({senderWallet, recipient, amount}) {
