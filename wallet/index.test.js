648bb891701c3342df60ace90b882dcc29bf3db5
dhmssb <dhmssb@192.168.1.11>
2024-05-05 19:01:43 +0700

    add transaction update amount and fix it

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index')
+const Transaction = require('./transaction')
 const {verifySignature} = require('../util')
 
 
@@ -39,6 +40,39 @@ describe('wallet', () => {
                     signature: new Wallet().sign(data)
                 })
             ).toBe(false)
+        });
+    });
+
+    describe('createTransaction(', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('should throws an error', () => {
+                expect(() => wallet.createTransaction({
+                    amount: 999999,
+                    recipients: 'this-recipient'
+                })).toThrow('Amount exceeds balance')
+            });
+        });
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient
+
+            beforeEach(() => {
+                amount = 50
+                recipient = 'this-recipient'
+                transaction = wallet.createTransaction({amount, recipient})
+            })
+
+            it('should creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true)
+            });
+
+            it('should matches the transaction input with wallet', () => {
+                expect(transaction.input.address).toEqual(wallet.publicKey)
+            });
+
+            it('should outputs the amount the recipient', () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount)
+            });
         });
     });
 });
