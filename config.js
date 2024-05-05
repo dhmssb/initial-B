2421fa9f5ed5df8ea3d8013a4065db595e148aa5
dhmssb <dhmssb@192.168.1.11>
2024-05-05 23:09:40 +0700

    add reward transaction

--- config.js
+++ config.js
@@ -11,5 +11,15 @@ const GENESIS_DATA = {
 }
 
 const STARTING_BALANCE = 1000
+const REWARD_INPUT = {
+    address: '*authorized-reward*'
+}
+const MINING_REWARD = 12
 
-module.exports = {GENESIS_DATA, MINE_RATE, STARTING_BALANCE}
+module.exports = {
+    GENESIS_DATA,
+    MINE_RATE,
+    STARTING_BALANCE,
+    REWARD_INPUT,
+    MINING_REWARD
+}
