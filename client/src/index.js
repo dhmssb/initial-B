0c6f1b7e5c566389f7affb6d6c68f0b7283e5e90
dhmssb <dhmssb@192.168.1.11>
2024-05-06 16:59:12 +0700

    add mine transaction to frontend

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,21 @@ import React from "react";
 import {render} from "react-dom";
 import App from "./components/App";
 import './index.css'
+import history from "./history";
+import {Route, Router, Switch} from "react-router-dom";
+import Blocks from "./components/Blocks";
+import ConductTransaction from "./components/ConductTransaction";
+import TransactionPool from "./components/TransactionPool";
 
 render(
-    <App/>,
+    <Router history={history}>
+        <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/blocks' component={Blocks}/>
+            <Route path='/conduct-transaction' component={ConductTransaction}/>
+            <Route path='/transaction-pool' component={TransactionPool}/>
+
+        </Switch>
+    </Router>,
     document.getElementById('root')
 )
